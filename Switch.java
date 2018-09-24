d1ebf157f610ea83c506f9f6ce72aed9f292bcf3
jayasrirethinam55 <43534619+jayasrirethinam55@users.noreply.github.com>
2018-09-24 19:09:57 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,39 @@
+package Jaya;
+
+public class Switchcase {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=4; i++)
+		{
+			switch(i)
+			{
+			case 1:
+				System.out.println("case 1");
+				i++;
+				break;
+			case 2 :
+				System.out.println("case 2");
+				break;
+			case 3 :
+				System.out.println("case 3");
+				break;
+			case 4:
+				System.out.println("case 4");
+				break;
+			case 5:
+				System.out.println("case 5");
+				break;
+				default:
+	             System.out.println("default");
+	             
+			}
+		}
+	}
+
+}
+
+Result:-
+
+case 1
+case 3
+case 4
