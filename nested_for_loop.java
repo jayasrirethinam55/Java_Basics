da937944fa23cd18d3dab5edaeb1746a0caed2a6
jayasrirethinam55 <43534619+jayasrirethinam55@users.noreply.github.com>
2018-09-24 19:02:43 +0530

    Create nested_for_loop.java

---
+++ nested_for_loop.java
@@ -0,0 +1,52 @@
+package Basics;
+
+public class CounterLogic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		for(int i=0; i<=2; i++)
+		{
+			for(int j=0; j<=2; j++)
+			{
+				for(int k=0; k<=2; k++)
+				{
+					++a;
+					System.out.println(a);
+				}
+			}
+		}
+
+	}
+
+}
+
+Result:-
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
