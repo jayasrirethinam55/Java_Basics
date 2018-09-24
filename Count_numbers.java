9c4122e3c2e4d4dc153951498679d6fc3b02b34e
jayasrirethinam55 <43534619+jayasrirethinam55@users.noreply.github.com>
2018-09-24 19:01:33 +0530

    Create Count_numbers.java

---
+++ Count_numbers.java
@@ -0,0 +1,33 @@
+package Basics;
+
+public class Counter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		for(int i=0; i<=2; i++)
+		{
+			
+				for(int k=0; k<=2; k++)
+				{
+					++a;
+					System.out.println(a);
+				}
+			
+		}
+
+	}
+
+}
+
+Result:-
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
