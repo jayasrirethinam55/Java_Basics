53cdd45ff01c443c881e29518d49cabaab22eb15
jayasrirethinam55 <43534619+jayasrirethinam55@users.noreply.github.com>
2018-09-24 19:03:59 +0530

    Create Check_password.java

---
+++ Check_password.java
@@ -0,0 +1,34 @@
+package Basics;
+
+import java.util.Scanner;  
+
+public class Password {	
+
+	public static void main(String[] args) {
+		
+		Scanner obj=new Scanner(System.in);
+         
+		System.out.println("Enter your Password");
+		
+		int number=obj.nextInt();
+		
+		int mypassword =1234;
+		
+		if(number==mypassword)
+		{
+		  System.out.println("Succees");
+		}
+		else
+		{
+			System.out.println("Check Password");
+		}
+		
+	}
+
+}
+
+
+Result:-
+Enter your Password
+1234
+Succees
