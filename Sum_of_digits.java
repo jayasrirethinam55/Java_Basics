1b636a20409e8ee6a3403b014b0e8f5d885b2793
jayasrirethinam55 <43534619+jayasrirethinam55@users.noreply.github.com>
2018-09-24 18:22:06 +0530

    Create Sum_of_digits.java

---
+++ Sum_of_digits.java
@@ -0,0 +1,28 @@
+package Basics;
+import java.util.Scanner;
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		
+		Scanner object = new Scanner(System.in);
+		System.out.println("Enter any number");
+		int number = object.nextInt();
+		int rem=0,sum = 0;
+		
+		while(number>0)
+		{
+			rem=number%10;
+			sum=sum+rem;
+			number=number/10;
+		}
+        System.out.println("The sum of digit is: "+sum);
+	}
+
+}
+
+
+Result:-
+
+Enter any number
+12345
+The sum of digit is: 15
